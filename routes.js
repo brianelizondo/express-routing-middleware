c243075370faaa2550a555bd8ac57cf01c7e81ba
Brian Elizondo <brian_elizondo@msn.com>
2022-06-19 20:56:45 -0400

    Express Routing & Middleware => Express Shopping List: 3. GET /items/:name - this route should display a single item’s name and price

--- routes.js
+++ routes.js
@@ -3,11 +3,12 @@ const router = new express.Router();
 const ExpressError = require('./expressError');
 let items = require('./fakeDb'); 
 
-
+// GET /items - this should render a list of shopping items.
 router.get('/', (req, res) => {
     res.json({ items });
 });
 
+// POST /items - this route should accept JSON data and add it to the shopping list
 router.post('/', (req, res) => {
     const newItem = {
         "name": req.body.name,
@@ -15,6 +16,15 @@ router.post('/', (req, res) => {
     };
     items.push(newItem);
     res.status(201).json({ newItem });
+});
+
+// GET /items/:name - this route should display a single item’s name and price
+router.get('/:name', (req, res) => {
+    const foundItem = items.find(item => item.name === req.params.name);
+    if(foundItem === undefined){
+        throw new ExpressError("Item not found", 404);
+    }
+    res.json({ item: foundItem });
 });
 
 module.exports = router;
